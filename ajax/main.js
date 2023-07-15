4577aa123cc6e8c48054e477d75651a7c387c6ed
Pierini Nahuel N <nahuel.pierini90@hotmail.com>
2023-07-15 18:46:56 -0300

    maquetado 3

---
+++ ajax/main.js
@@ -0,0 +1,39 @@
+let tab1= document.querySelector('.tab1');
+let tab2= document.querySelector('.tab2');
+let tab3= document.querySelector('.tab3');
+let tab4= document.querySelector('.tab4');
+let inf= document.querySelector('.content-info')
+
+
+function selectTab1(){
+    tab1.classList.add ('active')
+    tab2.classList.remove ('active')
+    tab3.classList.remove ('active')
+    tab4.classList.remove ('active')
+    alert('se toco')
+    general()
+}
+function selectTab2(){
+    tab2.classList.add ('active')
+    tab1.classList.remove ('active')
+    tab3.classList.remove ('active')
+    tab4.classList.remove ('active')
+    alert('se toco')
+    Bio()
+}
+function selectTab3(){
+    tab3.classList.add ('active')
+    tab1.classList.remove ('active')
+    tab2.classList.remove ('active')
+    tab4.classList.remove ('active')
+    alert('se toco')
+    skill()
+}
+function selectTab4(){
+    tab4.classList.add ('active')
+    tab1.classList.remove ('active')
+    tab2.classList.remove ('active')
+    tab3.classList.remove ('active')
+    alert('se toco')
+    relese()
+}
---
+++ ajax/templates.js
@@ -0,0 +1,61 @@
+function general(){
+    inf.innerHTML=`
+    <h5> ACÁ Va LA INFO GENERAL</h5>
+    <p>Lorem...</p>
+    `
+}
+
+
+
+function Bio(){
+    inf.innerHTML=`
+    <div class="card mb-3 justify-content-center" style="max-width: 540px;">
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img src="https://images.unsplash.com/photo-1531259683007-016a7b628fc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" class="img-fluid rounded-start" alt="Batman">
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+}
+
+function skill(){
+    inf.innerHTML=`
+    <div class="card mb-3 justify-content-center" style="max-width: 540px;">
+    <div class="row g-0">
+    <div class="col-md-4">
+    <img src="https://images.unsplash.com/photo-1531259683007-016a7b628fc3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80" class="img-fluid rounded-start" alt="Batman">
+  </div>
+      <div class="col-md-8 ">
+        <div class="card-body">
+          <h5 class="card-title">Skills or Habilities</h5>
+          <ul class="justify-content-center">
+            <li><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p></li>
+            <li><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p></li>
+            <li><p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p></li>
+            </ul>
+          
+          <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+        </div>
+      </div>
+    </div>
+  </div>
+    `
+}
+
+function relese(){
+    inf.innerHTML= `
+    <div class="card justify-content-center" style="width: 18rem;">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+    `
+}
